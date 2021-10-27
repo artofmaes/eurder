fc96722b428adf5827664b533f54c8febc3befdf
Jordi Maes <jordi19@live.be>
2021-10-27 13:37:12 +0200

    Initial commit

---
+++ src/test/java/com/artofmaes/eurder/EurderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artofmaes.eurder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
