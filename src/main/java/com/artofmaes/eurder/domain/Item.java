0a14f385b1c008ddac46fd0ca142e03eff807b14
Jordi Maes <jordi19@live.be>
2021-10-28 15:30:39 +0200

    Örder - Story 2 Making the item class

--- src/main/java/com/artofmaes/eurder/domain/Item.java
+++ src/main/java/com/artofmaes/eurder/domain/Item.java
@@ -1,4 +1,53 @@
 package com.artofmaes.eurder.domain;
 
 public class Item {
+    private final String name;
+    private final String description;
+    private final double price;
+    private final int stock;
+    private final StockSize stockSize;
+
+    public Item(String name, String description, double price, int stock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.stockSize = setStockSize(stock);
+    }
+
+    public StockSize getStockSize() {
+        return stockSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    private StockSize setStockSize(int stock) {
+        if(stock == 0){
+            return StockSize.STOCK_EMPTY;
+        }else if(stock > 0 && stock < 5){
+            return StockSize.STOCK_LOW;
+        } else if (stock >= 5 && stock < 10) {
+            return StockSize.STOCK_MEDIUM;
+        }else{
+            return StockSize.STOCK_HIGH;
+        }
+    }
+
+    public enum StockSize{
+        STOCK_HIGH,STOCK_MEDIUM,STOCK_LOW,STOCK_EMPTY
+    }
 }
